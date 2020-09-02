42cdb5736476e18e84bb024338ae8af4dcfe00f9
ted maker <tedmaker@teds-Mac-mini.attlocal.net>
2020-09-01 17:56:50 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     food app
+    </div>
+  );
+}
+
+export default App;
